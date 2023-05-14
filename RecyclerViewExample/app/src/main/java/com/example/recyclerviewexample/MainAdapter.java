5cc1ebee17ea42cb19c99d8f0575949e37298d3a
parkUiery <parkuiery@gmail.com>
2023-05-14 23:22:32 +0900

    ADD::RecyclerViewExample

---
+++ RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/MainAdapter.java
@@ -0,0 +1,55 @@
+package com.example.recyclerviewexample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    private ArrayList<String> locallDataSet;
+
+
+    //뷰 홀더 클래스
+   public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+        public TextView getTextView(){
+            return textView;
+        }
+    }
+
+    //생성자
+    public MainAdapter (ArrayList<String> dataSet) {
+       locallDataSet = dataSet;
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext())
+               .inflate(R.layout.recyclerview_item, parent, false);
+        MainAdapter.ViewHolder viewHolder = new MainAdapter.ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String text = locallDataSet.get(position);
+        holder.textView.setText(text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return locallDataSet.size();
+    }
+}
