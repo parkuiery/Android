e09e424ea18bb23c60edef027533def0665ff6cd
parkUiery <parkuiery@gmail.com>
2023-06-16 19:58:33 +0900

    perf :: 로그인 액티비티

---
+++ PracticeApp/app/src/main/java/com/example/practiceapp/login/LogInResponse.java
@@ -0,0 +1,14 @@
+package com.example.practiceapp.login;
+
+public class LogInResponse {
+    private String userId;
+    private String userPw;
+
+    public String getUserId(){
+        return userId;
+    }
+
+    public String getUserPw(){
+        return userPw;
+    }
+}
