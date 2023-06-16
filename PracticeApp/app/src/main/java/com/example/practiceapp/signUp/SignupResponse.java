e4d3520d62a8908dabde81a809557d1765632ee3
parkUiery <parkuiery@gmail.com>
2023-06-16 20:00:11 +0900

    perf :: 회원 가입 응답

---
+++ PracticeApp/app/src/main/java/com/example/practiceapp/signUp/SignupResponse.java
@@ -0,0 +1,20 @@
+package com.example.practiceapp.signUp;
+
+public class SignupResponse {
+    private String userId;
+    private String userPw;
+    private String userPwCheck;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserPw() {
+        return userPw;
+    }
+
+    public String getUserPwCheck() {
+        return userPwCheck;
+    }
+}
