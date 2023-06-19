4f232deb5d01269fe433bbcd99a9b6cdc3975865
parkUiery <parkuiery@gmail.com>
2023-06-19 19:25:19 +0900

    perf :: 바텀 네비게이션 추가

--- PracticeApp/app/src/main/java/com/example/practiceapp/home/HomeActivity.java
+++ PracticeApp/app/src/main/java/com/example/practiceapp/home/HomeActivity.java
@@ -1,16 +1,52 @@
 package com.example.practiceapp.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.os.Binder;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.practiceapp.R;
+import com.example.practiceapp.databinding.ActivityHomeBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        bottomNavigationView = binding.bottomNavi;
+
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new HomeFragment()).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
+                        break;
+                    case R.id.item2:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new CommunityFragment()).commit();
+                        break;
+                    case R.id.item3:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new SearchFragment()).commit();
+                        break;
+                    case R.id.item4:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new InfoFragment()).commit();
+                        break;
+
+                }
+                return true;
+            }
+        });
     }
 }
