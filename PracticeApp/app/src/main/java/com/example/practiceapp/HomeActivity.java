853fc31224f98b37e5e78eb38060ff5d4ca3e165
parkUiery <parkuiery@gmail.com>
2023-06-19 22:56:14 +0900

    refactor :: 코드 변경

--- PracticeApp/app/src/main/java/com/example/practiceapp/HomeActivity.java
+++ PracticeApp/app/src/main/java/com/example/practiceapp/HomeActivity.java
@@ -1,30 +1,35 @@
-package com.example.practiceapp.home;
+package com.example.practiceapp;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.practiceapp.databinding.ActivityHomeBinding;
+import com.example.practiceapp.home.CommunityFragment;
+import com.example.practiceapp.home.HomeFragment;
+import com.example.practiceapp.home.InfoFragment;
+import com.example.practiceapp.home.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity  {
+public class HomeActivity extends AppCompatActivity {
 
-    private ActivityHomeBinding binding;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
 
         getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new HomeFragment()).commit();
 
         HomeFragment homeFragment = new HomeFragment();
 
-        binding.bottomNavi.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
-                case :
-                    getSupportFragmentManager().beginTransaction().replace(R
-                            .id.main_frame, homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.item1:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new HomeFragment()).commit();
                     return true;
                 case R.id.item2:
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new CommunityFragment()).commit();
