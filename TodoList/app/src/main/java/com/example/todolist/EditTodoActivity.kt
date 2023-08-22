2b4183c6f3be69bdcd7647fe459650c9408b1a9a
parkUiery <parkuiery@gmail.com>
2023-08-22 22:36:09 +0900

    init :: 파일추가

---
+++ TodoList/app/src/main/java/com/example/todolist/EditTodoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditTodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_todo)
+    }
+}
