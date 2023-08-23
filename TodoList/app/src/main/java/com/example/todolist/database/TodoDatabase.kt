7c328322e5488b2a5964c9d95a830c63e5c40266
parkUiery <parkuiery@gmail.com>
2023-08-23 22:48:14 +0900

    init :: database

--- TodoList/app/src/main/java/com/example/todolist/database/TodoDatabase.kt
+++ TodoList/app/src/main/java/com/example/todolist/database/TodoDatabase.kt
@@ -1,4 +1,11 @@
 package com.example.todolist.database
 
-class TodoDatbase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.todolist.dao.TodoDao
+import com.example.todolist.dto.Todo
+
+@Database(entities = arrayOf(Todo::class), version = 1)
+abstract class TodoDatabase: RoomDatabase() {
+    abstract fun todoDao(): TodoDao
 }
