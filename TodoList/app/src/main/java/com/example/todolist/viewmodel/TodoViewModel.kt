d2ae9d6f0841c54187f7dd9a4bb11e03427487cd
parkUiery <parkuiery@gmail.com>
2023-08-24 07:36:31 +0900

    init :: viewmodel

--- TodoList/app/src/main/java/com/example/todolist/viewmodel/TodoViewModel.kt
+++ TodoList/app/src/main/java/com/example/todolist/viewmodel/TodoViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.todolist.viewmodel
 
-class TodoViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todolist.dto.Todo
+import com.example.todolist.repository.TodoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TodoViewModel: ViewModel() {
+    val todoList: LiveData<MutableList<Todo>>
+    private val todoRepository: TodoRepository = TodoRepository.get()
+
+    init {
+        todoList = todoRepository.list()
+    }
+
+    fun getOne(id: Long) = todoRepository.getTodo(id)
+
+    fun insert(dto: Todo) = viewModelScope.launch(Dispatchers.IO) {
+        todoRepository.update(dto)
+    }
+
+    fun delete(dto: Todo) = viewModelScope.launch(Dispatchers.IO) {
+        todoRepository.delete(dto)
+    }
 }
