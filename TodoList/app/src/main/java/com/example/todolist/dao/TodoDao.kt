b509fe5ffad9e78d74a515d642bab7a6260de051
parkUiery <parkuiery@gmail.com>
2023-08-23 22:44:34 +0900

    init :: dao

--- TodoList/app/src/main/java/com/example/todolist/dao/TodoDao.kt
+++ TodoList/app/src/main/java/com/example/todolist/dao/TodoDao.kt
@@ -1,4 +1,29 @@
 package com.example.todolist.dao
 
-class TodoDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.todolist.dto.Todo
+
+@Dao
+interface TodoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(dto: Todo)
+
+    @Query("select * from todoTable")
+    fun list(): LiveData<MutableList<Todo>>
+
+    @Query("select * from todoTable where id = (:id)")
+    fun selectOne(id: Long): Todo
+
+    @Update
+    suspend fun update(dto: Todo)
+
+    @Delete
+    fun delete(dto: Todo)
 }
