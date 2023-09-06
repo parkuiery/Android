e4cb628d13cdccc17861536533eb061ad1bb38cf
parkUiery <parkuiery@gmail.com>
2023-09-06 12:30:34 +0900

    🌱 :: 투두리스트 수정

--- TodoList/app/src/main/java/com/example/todolist/EditTodoActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/EditTodoActivity.kt
@@ -1,11 +1,47 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist.databinding.ActivityEditTodoBinding
+import com.example.todolist.dto.Todo
+import java.text.SimpleDateFormat
 
 class EditTodoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEditTodoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_todo)
+
+        binding = ActivityEditTodoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val type = intent.getStringExtra("type")
+
+        if(type.equals("ADD")) {
+            binding.btnSave.text = " 추가하기"
+        }else{
+            binding.btnSave.text = "수정하기"
+        }
+
+        binding.btnSave.setOnClickListener {
+            val title = binding.etTodoTitle.text.toString()
+            val content = binding.etTodoContent.text.toString()
+            val currentDate = SimpleDateFormat("yyyy-MM-dd HH:mm").format(System.currentTimeMillis())
+
+            if(type.equals("ADD")) {
+                if(title.isNotEmpty() && content.isNotEmpty()) {
+                    val todo = Todo(0, title, content, currentDate, false)
+                    val intent = Intent().apply {
+                        putExtra("todo",todo)
+                        putExtra("flag", 0)
+                    }
+                    setResult(RESULT_OK, intent)
+                    finish()
+                }
+                else{
+                    //수정
+                }
+            }
+        }
     }
 }
