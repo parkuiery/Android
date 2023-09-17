231f5229a02704a1bf39a91f64cf528c21036511
parkUiery <parkuiery@gmail.com>
2023-09-17 22:54:12 +0900

    뷰 모델

--- ClickPractice/app/src/main/java/com/amazing/android/clickpractice/MainViewModel.kt
+++ ClickPractice/app/src/main/java/com/amazing/android/clickpractice/MainViewModel.kt
@@ -1,4 +1,14 @@
 package com.amazing.android.clickpractice
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    val counter = MutableLiveData<Int>().apply { value = 0 }
+
+    fun incrementCounter() {
+        counter.value = (counter.value ?: 0) + 1
+    }
 }
