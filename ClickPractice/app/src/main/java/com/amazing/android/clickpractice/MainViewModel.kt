83c6432437ff436b25cd046675a2ff9fd45aaf75
parkUiery <parkuiery@gmail.com>
2023-09-18 09:11:10 +0900

    viewModel

--- ClickPractice/app/src/main/java/com/amazing/android/clickpractice/MainViewModel.kt
+++ ClickPractice/app/src/main/java/com/amazing/android/clickpractice/MainViewModel.kt
@@ -1,14 +1,41 @@
 package com.amazing.android.clickpractice
 
-import androidx.lifecycle.LiveData
+import android.os.CountDownTimer
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
+
+    private lateinit var countDownTimer: CountDownTimer
+    private var isClick = false
 
     val counter = MutableLiveData<Int>().apply { value = 0 }
+    val time = MutableLiveData<String>().apply { value = "0" }
 
     fun incrementCounter() {
-        counter.value = (counter.value ?: 0) + 1
+        if (isClick) counter.value = (counter.value ?: 0) + 1
+    }
+
+    fun checkTime() {
+        val countDownSeconds = 5
+
+        countDownTimer = object : CountDownTimer((countDownSeconds * 1000).toLong(), 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsRemaining = (millisUntilFinished / 1000).toInt()
+                time.value = secondsRemaining.toString()
+                isClick = true
+            }
+
+            override fun onFinish() {
+                isClick = false
+            }
+        }
+
+        countDownTimer.start()
+
+    }
+
+    fun set() {
+        counter.value = 0
     }
 }
