06f1d9b1513bdc10d17e89ec651285ed630903b3
parkUiery <parkuiery@gmail.com>
2023-12-12 19:18:56 +0900

    init

--- ComposePractice/app/src/main/java/com/uiel/composepractice/MainActivity.kt
+++ ComposePractice/app/src/main/java/com/uiel/composepractice/MainActivity.kt
@@ -3,13 +3,24 @@ package com.uiel.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.uiel.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +34,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    Btn("업데이트 상황")
                 }
             }
         }
@@ -34,7 +46,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+            .padding(10.dp)
     )
+}
+
+@Composable
+fun Btn(name: String, modifier: Modifier = Modifier) {
+    Button(onClick = {},
+        contentPadding = PaddingValues(12.dp),
+        modifier = Modifier.fillMaxWidth(0.8f)
+            .height(50.dp)
+            .background(Color.Gray)
+        ) {
+        Text(text = name)
+    }
 }
 
 @Preview(showBackground = true)
