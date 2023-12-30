7aab9a63c1512ec5512623a5553b52faa7c2521b
uielPark <parkuiery@gmail.com>
2023-12-30 20:09:58 +0900

    🌱 :: 깃허브 api

--- MultiplatformPractice/shared/src/commonMain/kotlin/com/example/multiplatformpractice/GithubDataSource.kt
+++ MultiplatformPractice/shared/src/commonMain/kotlin/com/example/multiplatformpractice/GithubDataSource.kt
@@ -1,4 +1,30 @@
 package com.example.multiplatformpractice
 
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import kotlin.coroutines.coroutineContext
+
 class GithubDataSource {
+
+    private val client = HttpClient() {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                coerceInputValues = true
+            })
+        }
+    }
+
+    suspend fun getGithubUserInfo(userId: String) {
+        withContext(Dispatchers.IO) {
+            return@withContext client
+                .get("https://api.github.com/users/$userId")
+        }
+    }
 }
