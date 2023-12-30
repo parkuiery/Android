9535b4c24ee1529f9d4bd4bb88431da3f1f50703
uielPark <parkuiery@gmail.com>
2023-12-30 20:10:16 +0900

    🌱 :: repository

--- MultiplatformPractice/shared/src/commonMain/kotlin/com/example/multiplatformpractice/Repository.kt
+++ MultiplatformPractice/shared/src/commonMain/kotlin/com/example/multiplatformpractice/Repository.kt
@@ -1,4 +1,18 @@
 package com.example.multiplatformpractice
 
 class Repository {
+
+    private val githubDataSource = GithubDataSource()
+
+    private val stringDataSource = StringDataSource()
+
+    suspend fun getGitHubRepository(string: String) {
+        return githubDataSource.getGithubUserInfo(string)
+    }
+
+    fun getSavedString() : String =
+        stringDataSource.getSavedString()
+
+    fun saveString(string: String) =
+        stringDataSource.saveString(string)
 }
