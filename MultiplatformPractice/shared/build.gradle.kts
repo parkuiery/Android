397cd6e9ab0243b4875e132b90faea494287c1f3
uielPark <parkuiery@gmail.com>
2023-12-30 20:09:13 +0900

    ⚙️ :: 라이브러리 추가

--- MultiplatformPractice/shared/build.gradle.kts
+++ MultiplatformPractice/shared/build.gradle.kts
@@ -25,10 +25,21 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+            implementation("io.ktor:ktor-client-core:2.3.1")
+            implementation("io.ktor:ktor-client-cio:2.3.1")
+            implementation("io.ktor:ktor-client-content-negotiation:2.3.1")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.1")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+
+        }
+        androidMain.dependencies {
+            implementation("io.insert-koin:koin-android:3.4.2")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:2.3.1")
         }
     }
 }
@@ -39,4 +50,9 @@ android {
     defaultConfig {
         minSdk = 33
     }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:+")
+    implementation("androidx.core:core-ktx:+")
+    implementation("androidx.core:core-ktx:+")
 }
