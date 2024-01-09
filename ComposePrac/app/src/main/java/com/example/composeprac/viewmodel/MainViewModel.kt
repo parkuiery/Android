72f67f7389ac256a44f8f10a509c2c6f036bc15b
uielPark <parkuiery@gmail.com>
2024-01-09 22:42:49 +0900

    ♻️ :: 코드 변경

--- ComposePrac/app/src/main/java/com/example/composeprac/viewmodel/MainViewModel.kt
+++ ComposePrac/app/src/main/java/com/example/composeprac/viewmodel/MainViewModel.kt
@@ -1,15 +1,25 @@
 package com.example.composeprac.viewmodel
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.composeprac.LoginState
 
 class MainViewModel : ViewModel(){
-    private val _email: MutableLiveData<String> = MutableLiveData()
-    val email : LiveData<String> get() = _email
+    private val _loginState: MutableLiveData<LoginState> = MutableLiveData(LoginState())
+    val loginState: LiveData<LoginState> = _loginState
+    //val email : LiveData<LoginState> get() = loginState
+    //val password : LiveData<LoginState> get() = loginState
+    //val loginState : LiveData<LoginState> = _loginState
 
     fun updateEmail(email : String) {
-        _email.postValue(email)
+        _loginState.postValue(loginState.value?.copy(email = email))
+    }
+
+    fun updatePassword(password : String) {
+        _loginState.postValue(loginState.value?.copy(password = password))
     }
 
 
