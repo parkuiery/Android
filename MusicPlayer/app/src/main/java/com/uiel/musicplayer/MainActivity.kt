17698937c82ba81c3c6055e52d30874df89e0ea4
uielPark <parkuiery@gmail.com>
2024-02-01 20:08:57 +0900

    ☁️ :: 네비게이션 추가

--- MusicPlayer/app/src/main/java/com/uiel/musicplayer/MainActivity.kt
+++ MusicPlayer/app/src/main/java/com/uiel/musicplayer/MainActivity.kt
@@ -1,45 +1,16 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
 package com.uiel.musicplayer
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material.icons.outlined.ArrowForward
-import androidx.compose.material.icons.outlined.Clear
-import androidx.compose.material.icons.outlined.KeyboardArrowDown
-import androidx.compose.material.icons.outlined.MoreVert
-import androidx.compose.material.icons.outlined.PlayArrow
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.uiel.musicplayer.ui.music.MusicScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.uiel.musicplayer.ui.home.HomeScreen
+import com.uiel.musicplayer.ui.playlist.PlayListScreen
 import com.uiel.musicplayer.ui.theme.MusicPlayerTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,11 +18,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MusicPlayerTheme {
-                MusicScreen()
+                MusicApp()
             }
         }
     }
 }
 
-
-
+@Composable
+fun MusicApp() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "home",
+    ) {
+        composable("home") {
+            HomeScreen(
+                openPlayList = {
+                    navController.navigate("play_list")
+                },
+            )
+        }
+        composable("play_list") {
+            PlayListScreen(
+                navigateUp = {
+                    //navController.popBackStack()
+                    navController::navigateUp
+                },
+            )
+        }
+    }
+}
