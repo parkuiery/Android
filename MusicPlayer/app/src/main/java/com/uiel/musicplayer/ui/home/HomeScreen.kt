67af25ac67a3128de74f7f74a59420d0deff3fe6
uielPark <parkuiery@gmail.com>
2024-02-22 15:49:03 +0900

    refactor :: viewModel 연결 및 공백 제거

--- MusicPlayer/app/src/main/java/com/uiel/musicplayer/ui/home/HomeScreen.kt
+++ MusicPlayer/app/src/main/java/com/uiel/musicplayer/ui/home/HomeScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.BottomAppBar
@@ -20,22 +19,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.uiel.musicplayer.R
-import com.uiel.musicplayer.data.playlist.PlayList
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     openPlayList: (
-         playListId: Long,
-         playListTitle: String,
+        playListId: Long,
+        playListTitle: String,
     ) -> Unit,
     onShowCurrentPlayingModal: () -> Unit,
+    viewModel: HomeViewModel = hiltViewModel()
 ) {
+    val playList by remember {
+        mutableStateOf(viewModel.fetchPlayLists())
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -99,7 +104,7 @@ fun HomeScreen(
                 .padding(paddingValues),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {// RecyclerView 와 유사
-            items(playListItems) {item ->
+            items(playList) { item ->
                 ElevatedCard(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -121,7 +126,7 @@ fun HomeScreen(
     }
 }
 
-val playListItems = listOf(
+/*val playListItems = listOf(
     PlayList(
         id = 1,
         title = "좋아요 표시한 곡",
@@ -147,7 +152,7 @@ val playListItems = listOf(
         title = "좋아요 표시한 곡",
         madeBy = "박의엘"
     ),
-)
+)*/
 
 @SuppressLint("PrivateResource")
 val homeBottomAppBarItems: List<BottomAppBarItem> = listOf(
